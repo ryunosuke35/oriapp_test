2445afbb7b30bec10d1949874bede89110b6be7e
ryunosuke35 <6bev2103@gmail.com>
2021-07-27 01:40:27 +0900

    モデルの作成

---
+++ db/migrate/20210726152220_add_column_to_user_table.rb
@@ -0,0 +1,6 @@
+class AddColumnToUserTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :level, :integer, default: 1
+    add_column :users, :experience_point, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_144724) do
+ActiveRecord::Schema.define(version: 2021_07_26_161113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "level_settings", force: :cascade do |t|
+    t.integer "level"
+    t.integer "thresold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -35,6 +42,8 @@ ActiveRecord::Schema.define(version: 2021_07_26_144724) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "level", default: 1
+    t.integer "experience_point", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
